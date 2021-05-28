33a67f28ae5a0a0aabf7b3da7966d91b814b0f44
Daniel Bogod <daniel.bogod@historicengland.org.uk>
2021-05-28 18:08:17 +0100

    Resolve useEffect dependency issue

--- src/components/molecules/video.js
+++ src/components/molecules/video.js
@@ -37,7 +37,7 @@ const Video = ({ video }) => {
     }
 
     return () => window.removeEventListener("message", window.ssFrmHeightAdjustFunc);
-  }, [isYouTubeVideo]);
+  }, [isShopShareTvVideo]);
 
 
   return (
